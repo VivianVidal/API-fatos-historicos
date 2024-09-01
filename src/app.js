e216a480a42f0d850ebc2818e9b4172c3f4dde32
Vivian <viviancristinavidal@gmail.com>
2024-09-01 15:15:21 -0300

    :recycle: refactor(app.js && src/models/fatos): Alterando exports e imports

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-const express = require('express');
+import express from 'express';
+import { buscaFatoPorAno } from '../src/services/fatosService.js';
+
 const app = express();
 
-app.get('/', (req,res) => {
-    res.json({mensagem: 'Hello World'});
-})
+app.get('/', (req, res) => {
+    const ano = req.query.ano;
+    const buscar = buscaFatoPorAno(ano);
+    res.json(buscar);
+});
 
-app.listen(8080, ()=> {
+app.listen(8080, () => {
     console.log("Servidor rodando na porta 8080");
-})
+});
