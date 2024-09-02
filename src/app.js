79afc6824604b7324eef9e6bb91398fef2c86588
Vivian <viviancristinavidal@gmail.com>
2024-09-01 21:52:56 -0300

    :sparkles: feat(src/services/fatosService.js && src/app.js): Adicionando tratamento de erros

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 import express from 'express';
-import { buscaFatoPorAno } from '../src/services/fatosService.js';
+import { buscaFatoPorAno, validaAno } from '../src/services/fatosService.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    const ano = req.query.ano;
-    const buscar = buscaFatoPorAno(ano);
-    res.json(buscar);
+    try {
+        const ano = req.query.ano;
+        if (validaAno(ano)) {
+            const buscar = buscaFatoPorAno(ano);
+            if (buscar == null || buscar == undefined) {
+                res.status(404).json({ erro: 'Nenhum fato histórico cadastrado para esse ano' })
+            } else {
+                res.json(buscar);
+            }
+        } else {
+            res.status(400).json({ erro: 'Ano inválido' })
+        }
+    } catch (error) {
+        console.error('Ocorreu um erro:', error);
+        res.status(500).json({ error: 'Erro interno' })
+    }
 });
 
 app.listen(8080, () => {
